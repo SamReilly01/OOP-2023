922a63f1006ee25b2fc8c88be3675ecba29f6398
SamReilly01 <c21373633@mytudublin.ie>
2023-02-24 11:24:40 +0000

    lab5 done

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -8,57 +8,166 @@ import processing.core.PApplet;
 
 public class Audio1 extends PApplet
 {
-
     Minim minim;
-    AudioInput ai;
     AudioPlayer ap;
+    AudioInput ai;
     AudioBuffer ab;
 
-	public void settings()
-	{
-		size(1024, 500);
+    int mode = 0;
+
+    float y = 0;
+    float smoothedY = 0;
+    float smoothedAmplitude = 0;
+
+    public void keyPressed() {
+		if (key >= '0' && key <= '9') {
+			mode = key - '0';
+		}
+		if (keyCode == ' ') {
+            if (ap.isPlaying()) {
+                ap.pause();
+            } else {
+                ap.rewind();
+                ap.play();
+            }
+        }
 	}
 
-    int frameSize = 1024;
+    public void settings()
+    {
+        size(1024, 1000, P3D);
+        //fullScreen(P3D, SPAN);
+    }
 
-	public void setup() {
-		colorMode(HSB);
-		background(0);
-
+    public void setup()
+    {
         minim = new Minim(this);
+        // Uncomment this to use the microphone
+        // ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
+        // ab = ai.mix; 
 
-        ai = minim.getLineIn(Minim.MONO, frameSize, 44100, 16);
-        ab = ai.mix;
-		
-		smooth();
-		
-	}
+        // And comment the next two lines out
+        ap = minim.loadFile("heroplanet.mp3", 1024);
+        ap.play();
+        ab = ap.mix;
+        colorMode(HSB);
 
-	
-	
-	public void draw()
-	{	
-		background(0);
-        stroke(255);
+        y = height / 2;
+        smoothedY = y;
 
-        float half = height / 2;
-        float cgap = 255 / (float)ab.size();
+    }
 
-        float total = 0;
+    float off = 0;
+
+    public void draw()
+    {
+        //background(0);
+        //half the height of the screen
+        float halfH = height / 2;
+        //half the width of the screen
+        float average = 0;
+        //average of the samples within the buffer
+        float sum = 0;
+        //increment the offset
+        off += 1;
+
+        // Calculate sum and average of the samples
+        // Also lerp each element of buffer;
         for(int i = 0 ; i < ab.size() ; i ++)
         {
-            total += abs(ab.get(i));
-            stroke(cgap * i, 255, 255);
-            line(i, half, i , half + ab.get(i) * half); 
+            sum += abs(ab.get(i));
         }
-        float average = total / (float) ab.size();
+        average= sum / (float) ab.size();
 
-        
-        float r = average * 200;
-        lerpedR = lerp(lerpedR, r, 0.1f);
+        smoothedAmplitude = lerp(smoothedAmplitude, average, 0.1f);
 
-        circle(100, 200, lerpedR);
-	}
+        float cx = width / 2;
+        float cy = height / 2;
 
-    float lerpedR = 0;
+        switch (mode) {
+			case 0:
+                background(0);
+                for(int i = 0 ; i < ab.size() ; i ++)
+                {
+                    //float c = map(ab.get(i), -1, 1, 0, 255);
+                    float c = map(i, 0, ab.size(), 0, 255);
+                    stroke(c, 255, 255);
+                    float f = ab.get(i) * halfH;
+                    line(i, halfH + f, i, halfH - f);                    
+                }
+                break;
+        case 1:
+            background(0);  
+            for (int i = 0; i < ab.size(); i++) 
+            {
+                float c = map(i, 0, ab.size(), 0, 255);
+                stroke(c, 255, 255);
+                float f = ab.get(i) * halfH;
+                line(i, halfH + f, halfH - f, i);
+            }
+            break;
+        case 2:
+            background(0);
+            for(int i = 0; i < ab.size(); i++)
+            {
+                float c = map(i, 0, ab.size(), 0, 255);
+                stroke(c, 255, 255);
+                float f = ab.get(i) * height;
+                line(i, 0 + f, i, 0 );
+                line(i, height + f, i, height - f);
+                line(width + f, i, width - f, i);
+                line(f, i, -f, i);
+            }
+
+            break;
+        case 3:
+            background(0);
+            for(int i = 0; i < ab.size(); i++)
+            {
+                float f = ab.get(i) * halfH;
+                stroke(f, 255, 255);
+                fill(0);
+                circle(cx, cy, smoothedAmplitude * height);
+            }
+
+            break;
+        case 4:
+            background(0);
+            for (int i = 0; i < ab.size(); i++) {
+                float f = ab.get(i) * halfH;
+                stroke(f, 255, 255);
+                fill(0);
+                rectMode(CENTER);
+                rect(cx, cy, smoothedAmplitude * height, smoothedAmplitude * height);
+            }
+
+            break;
+        case 5:
+            float halfW = width / 2;
+            background(0);
+            for(int i = 0; i < ab.size(); i++)
+            {
+                float f = ab.get(i) * halfH;
+                stroke(f, 255, 255);
+                fill(0);
+                line(cx + halfW, halfH + f, cx - halfW, halfH - f);
+                line(f + halfW, halfH + cx, f - halfW, halfH - cx);
+                line(halfH - cx, f - halfW, halfH + cx, f + halfW);
+                line(i, halfH + f, i, halfH - f);  
+                rectMode(CENTER);
+                rect(cx / 2, cy / 2, smoothedAmplitude * height * 2, smoothedAmplitude * height * 2);
+                rect((cx / 2) + halfW, cy / 2, smoothedAmplitude * height * 2, smoothedAmplitude * height * 2);
+                rect(cx / 2, (cy / 2) + halfH, smoothedAmplitude * height * 2, smoothedAmplitude * height * 2);
+                rect((cx / 2) + halfW, (cy / 2) + halfH, smoothedAmplitude * height * 2, smoothedAmplitude * height * 2);
+                circle(cx / 2, cy / 2, smoothedAmplitude * height * 2);
+                circle((cx / 2) + halfW, cy / 2, smoothedAmplitude * height * 2);
+                circle(cx / 2, (cy / 2) + halfH, smoothedAmplitude * height * 2);
+                circle((cx / 2) + halfW, (cy / 2) + halfH, smoothedAmplitude * height * 2); 
+            }
+            
+            break;
+        }
+
+
+    }        
 }
